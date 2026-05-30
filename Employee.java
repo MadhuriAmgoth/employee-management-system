f1c476001f6560bff22fd7d3411ba8563571afce
MadhuriAmgoth <119039397+MadhuriAmgoth@users.noreply.github.com>
2026-05-31 01:50:40 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,6 @@
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Employee Management System");
+        System.out.println("Created by Madhuri Amgoth");
+    }
+}
